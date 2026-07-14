de3e5902390e0560b08abbdce1bbfb878ccaafc0
Nitesh <niteshtirkey4@gmail.com>
2026-07-14 12:02:41 +0530

    first setup restapi create

---
+++ spring-boot-master/codingShuttle/src/test/java/com/codingShuttle/CodingShuttleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingShuttle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingShuttleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
