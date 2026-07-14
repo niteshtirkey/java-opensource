be75a2977f286333ac4792f7d1f6f1b22bfe0117
Nitesh <niteshtirkey4@gmail.com>
2026-07-14 12:02:41 +0530

    first setup restapi create

---
+++ spring-boot-master/codingShuttle/src/main/java/com/codingShuttle/CodingShuttleApplication.java
@@ -0,0 +1,13 @@
+package com.codingShuttle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingShuttleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingShuttleApplication.class, args);
+	}
+
+}
