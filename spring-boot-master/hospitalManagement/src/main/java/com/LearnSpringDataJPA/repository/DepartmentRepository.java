4cc9c70f11825fcea74822cfbf1a25d55eeb68d1
Your Name <you@example.com>
2026-08-22 23:40:18 +0530

    Move configuration files to config-server directory

---
+++ spring-boot-master/hospitalManagement/src/main/java/com/LearnSpringDataJPA/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.LearnSpringDataJPA.repository;
+
+import com.LearnSpringDataJPA.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
---
+++ spring-boot-master/hospitalManagement/src/main/java/com/LearnSpringDataJPA/repository/DoctorRepository.java
@@ -0,0 +1,7 @@
+package com.LearnSpringDataJPA.repository;
+
+import com.LearnSpringDataJPA.entity.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+}
