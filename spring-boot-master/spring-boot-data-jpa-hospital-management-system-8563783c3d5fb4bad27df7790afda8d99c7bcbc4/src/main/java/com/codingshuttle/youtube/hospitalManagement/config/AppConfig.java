0f3e1e976a817abce336b3baaf0ac2c4367964d7
Your Name <you@example.com>
2026-09-02 12:55:15 +0530

    Spring Security 6 with JWT Authentication

--- spring-boot-master/spring-boot-data-jpa-hospital-management-system-8563783c3d5fb4bad27df7790afda8d99c7bcbc4/src/main/java/com/codingshuttle/youtube/hospitalManagement/config/AppConfig.java
+++ spring-boot-master/spring-boot-data-jpa-hospital-management-system-8563783c3d5fb4bad27df7790afda8d99c7bcbc4/src/main/java/com/codingshuttle/youtube/hospitalManagement/config/AppConfig.java
@@ -3,8 +3,12 @@ package com.codingshuttle.youtube.hospitalManagement.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class AppConfig {
@@ -17,5 +21,18 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+//    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("admin")
+                .password(passwordEncoder().encode("pass"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user2 = User.withUsername("patient")
+                .password(passwordEncoder().encode("pass"))
+                .roles("PATIENT")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
