ac996fe60ebcc2900e45364426f69a46ce83e609
Nitesh Tirkey <niteshtirkey4@gmail.com>
2026-09-02 12:55:15 +0530

    Spring Security 6 with JWT Authentication

---
+++ spring-boot-master/spring-boot-data-jpa-hospital-management-system-8563783c3d5fb4bad27df7790afda8d99c7bcbc4/src/main/java/com/codingshuttle/youtube/hospitalManagement/security/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.codingshuttle.youtube.hospitalManagement.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrfConfig->csrfConfig.disable())
+                .sessionManagement(sessionConfig->sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/public/**","/auth/**").permitAll()
+//                                .requestMatchers("/admin/**").hasRole("ADMIN")
+//                                .requestMatchers("/doctors/**").hasAnyRole("ADMIN", "DOCTORS")
+                                .anyRequest().authenticated()
+                )
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+//                .formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
+
