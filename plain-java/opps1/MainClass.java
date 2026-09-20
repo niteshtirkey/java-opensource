2542fcf0d6a1a3460984196588f2c149a51a726b
Nitesh Tirkey <niteshtirkey4@gmail.com>
2026-09-20 11:24:55 +0530

    basic opps

---
+++ plain-java/opps1/MainClass.java
@@ -0,0 +1,49 @@
+package opps1;
+
+public class MainClass {
+    public static void main(String[] args) {
+        // Dog d1 = new Dog();
+
+        // d1.name = "Tommy";
+        // d1.bark();
+
+        // Dog d2 = new Dog();
+        // d2.name = "Leo";
+        // d2.walk();
+
+        Complex num1 = new Complex();
+
+        num1.a = 3;
+        num1.b = 5;
+        num1.print();
+
+    }
+
+}
+
+class Complex {
+    int a, b;
+
+    void print() {
+        System.out.println(a + " + " + b + "i");
+    }
+}
+
+class Dog {
+
+    String name;
+    String age;
+    String color;
+
+    void walk() {
+        System.out.println(name + " is walking.");
+    }
+
+    void bark() {
+        System.out.println(name + " is barking.");
+    }
+}
+
+class Cat {
+
+}
---
+++ plain-java/opps1/MethodOverLoading.java
@@ -0,0 +1,27 @@
+package opps1;
+
+public class MethodOverLoading {
+    public static void main(String[] args) {
+        Greet obj = new Greet();
+        obj.greetings();
+        obj.greetings("Nitesh");
+        obj.greetings("mukesh",5);
+    }
+}
+
+class Greet {
+    void greetings() {
+        System.out.println("Helllo, good morning");
+    }
+
+    void greetings(String name) {
+        System.out.println("Hello " + name + " good moring");
+    }
+
+    void greetings(String name, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println("Hello " + name + " How are you?");
+        }
+
+    }
+}
